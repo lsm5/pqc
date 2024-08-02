48ae3a3a413502dcf0f914588eae6126a2b2d51a
Lokesh Mandvekar <lsm5@fedoraproject.org>
2024-08-02 16:05:35 -0400

    fix test script
    Signed-off-by: Lokesh Mandvekar <lsm5@fedoraproject.org>

--- tests.sh
+++ tests.sh
@@ -9,10 +9,11 @@ openssl list -providers | grep 'name: OpenSSL OQS Provider'
 
 #OpenSSL client and server tests
 
-openssl s_server -key root.key -cert root.crt & #run the server
+#run the server
+openssl s_server -key root.key -cert root.crt &
 
 # Default connection to X25519-KYBER768
-openssl s_client -connect localhost:4433 -tls1_3 -trace | grep ‘NamedGroup: X25519Kyber768Draft00 (25497)’
+openssl s_client -connect localhost:4433 -tls1_3 -trace | grep 'NamedGroup: X25519Kyber768Draft00 (25497)'
 
 #Specify the group explicitly
 openssl s_client  -groups p256_kyber768 -connect localhost:4433 -tls1_3 -trace | grep 'NamedGroup: SecP256r1Kyber768Draft00 (25498)'
